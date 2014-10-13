4297b0718471d504b947f7f0e28056232e2bec4a
Lindsey Bieda <hazardous.narayan@gmail.com>
2014-10-13 17:41:48 -0400

    I made a terrible chrome extension

---
+++ content_script.js
@@ -0,0 +1,39 @@
+var regex_sjw = /\bsjw\b/gi // i flag makes it case insensitive
+var regex_spelled_out = /\bsocial justice warrior\b/gi
+var roles = Array("Paladin", "Mage", "Rogue", "Thief", "Assassin", "Warlock", "Wizard", "Ranger", "Cleric", "Priest", "Knight");
+walk(document.body);
+
+
+
+function walk(node)
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+
+	var child, next;
+
+	switch ( node.nodeType )
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child )
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			if(regex_sjw.test(node.nodeValue)){
+				node.data = node.data.replace(regex_sjw, "PB&J");
+			}
+
+			if(regex_spelled_out.test(node.nodeValue)){
+				var role = roles[Math.floor(Math.random()*roles.length)];
+				node.data = node.data.replace(regex_spelled_out, "Social Justice " + role);
+			}
+	}
+}
